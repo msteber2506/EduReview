9df8389dd0d34ddcf4e4981ab414733b12bf951c
mustafasuhateber <msteber2506@gmail.com>
2023-02-21 21:26:49 -0500

    Updated Files

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
 
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 
+const userSchema = mongoose.Schema(
+    {
+        username: String,
+        password: String,
+        email: String,
+    }
+)
+
+const User = mongoose.model('User', userSchema);
+
+
+
+
 app.get('/', (req,res) =>{
     res.render('home')
+})
+
+app.get('/contribute', (req,res) =>{
+    res.render('contribute')
 })
 
 app.get('')
