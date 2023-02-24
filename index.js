0b8ec26a083a3785439caca5bc7ce1e7ddf7750e
mustafasuhateber <msteber2506@gmail.com>
2023-02-23 23:42:39 -0500

    Completed register and login system

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/test");
+mongoose.connect("mongodb://localhost:27017/edureview");
 const User = require("./models/user");
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,14 +24,13 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", async function (req, res) {
-  const user = await User.findOne({ username: req.body.username });
   try {
     const user = await User.findOne({ username: req.body.username });
     if (user) {
       const result = req.body.password == user.password;
-      if(result){
-        res.send("SUCCESSFULL LOGIN");
-      }else{
+      if (result) {
+        res.redirect("/");
+      } else {
         res.send("INCORRECT PASSWORD");
       }
     } else {
@@ -43,11 +42,27 @@ app.post("/login", async function (req, res) {
 });
 
 app.get("/register", (req, res) => {
-    res.render("register");
-})
+  res.render("register");
+});
 
-
-
+app.post("/register", async function (req, res) {
+  try {
+    const exists = await User.findOne({ username: req.body.username });
+    if (!exists) {
+      const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      res.send("ACCOUNT CREATED");
+      res.redirect("home");
+    } else {
+      res.send("USERNAME ALREADY EXISTS");
+    }
+  } catch (e) {
+    res.send("ERROR");
+  }
+});
 
 app.get("");
 
